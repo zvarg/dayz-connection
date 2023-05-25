26db64caf5ef988e23d701b3a1ba31f6a4decd4e
Zoltán Varga <vargazolli@gmail.com>
2023-05-25 20:15:09 +0200

    Trace route an ping implementation.

--- DayzConnection/MainPage.xaml.cs
+++ DayzConnection/MainPage.xaml.cs
@@ -1,24 +1,133 @@
-﻿namespace DayzConnection;
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace DayzConnection;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private string ResolveHostname(string hostNameOrAddress)
+    {
+        IPHostEntry hostEntry;
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        try
+        {
+            hostEntry = Dns.GetHostEntry(hostNameOrAddress);
+        }
+        catch
+        {
+            return hostNameOrAddress;
+        }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        return hostEntry.HostName;
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void Refresh(string text)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            PingResultEditor.Text += text + Environment.NewLine;
+
+            SemanticScreenReader.Announce(PingResultEditor.Text);
+        });
+    }
+
+    private void OnTracerouteClicked(object sender, EventArgs e)
+    {
+        PingResultEditor.Text = "";
+        SemanticScreenReader.Announce(PingResultEditor.Text);
+
+        var hostname = "195.18.27.235";
+        var maxTtl = 30;
+        var timeout = 1000;
+        const int bufferSize = 32;
+        var buffer = new byte[bufferSize];
+        new Random().NextBytes(buffer);
+
+        var stopwatch = new Stopwatch();
+
+        var pinger = new Ping();
+
+        _ = Task.Factory.StartNew(() =>
+        {
+            for (int ttl = 1; ttl <= maxTtl; ttl++)
+            {
+                stopwatch.Start();
+
+                var pingOptions = new PingOptions(ttl, true);
+                PingReply reply = null;
+
+                try
+                {
+                    reply = pinger.Send(hostname, timeout, buffer, pingOptions);
+                }
+                catch (Exception exception)
+                {
+                    Refresh(exception.Message);
+
+                    break;
+                }
+
+                if (reply != null)
+                {
+                    switch (reply.Status)
+                    {
+                        case IPStatus.TtlExpired:
+                            {
+                                var hostname = ResolveHostname(reply.Address.ToString());
+                                Refresh($"[{ttl}] {hostname} in {stopwatch.ElapsedMilliseconds} ms");
+                                stopwatch.Restart();
+                                continue;
+                            }
+                        case IPStatus.TimedOut:
+                            {
+                                Refresh($"[{ttl}] Timeout.");
+                                stopwatch.Restart();
+                                continue;
+                            }
+                        case IPStatus.Success:
+                            {
+                                stopwatch.Stop();
+                                Refresh($"[{ttl}] Successful trace route to {hostname} in {stopwatch.ElapsedMilliseconds} ms");
+                            }
+                            break;
+                        default:
+                            {                                
+                                Refresh($"Default  - Time: {stopwatch.ElapsedMilliseconds} ms");
+                                stopwatch.Restart();
+                            }
+                            break;
+                    }
+                }
+                break;
+            }
+        });
+    }
+
+    private void OnPingClicked(object sender, EventArgs e)
+    {
+        PingResultEditor.Text = "";
+        SemanticScreenReader.Announce(PingResultEditor.Text);
+
+        var pinger = new Ping();
+
+        var address = "195.18.27.235";
+        var timeout = 1000;
+
+        const int bufferSize = 32;
+        var buffer = new byte[bufferSize];
+        new Random().NextBytes(buffer);
+
+        PingReply reply = pinger.Send(address, timeout, buffer);
+
+        var hostname = ResolveHostname(reply.Address.ToString());
+
+        Refresh($"Reply from {hostname} ({address}) in {reply.RoundtripTime} ms");        
+    }
 }
 
